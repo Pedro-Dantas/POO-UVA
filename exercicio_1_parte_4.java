3da68883895159b207585bcdfddd72ef45cc41ad
Pèdro Dantas <dev.pedrodantas@gmail.com>
2020-09-23 22:53:16 -0300

    Exercicio 1 parte 4-2 :ok_hand:

---
+++ exercicio_1_parte_4.java
@@ -0,0 +1,16 @@
+package Aula_2020_09_14_m_exercicio_1;
+
+/**
+ *
+ * @author pedro.dantas
+ */
+public class exercicio_1_parte_4
+{
+    public static void main(String[] args)
+    {
+        PenDrive pendrive = new PenDrive();
+       
+        pendrive.entrada();
+        pendrive.imprimir();
+    }   
+}
